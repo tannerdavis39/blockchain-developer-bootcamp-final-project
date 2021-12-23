30ef3ad2fc1c9ec9a8c41289ebf902ee60b35051
tannerdavis39 <90158976+tannerdavis39@users.noreply.github.com>
2021-12-23 13:02:46 -0500

    altered unit tests

--- contracts/contract2.sol
+++ contracts/contract2.sol
@@ -1,65 +1,15 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.7.0;
+pragma solidity 0.8.10;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-import "@openzeppelin/contracts/access/Ownable.sol";
+    contract contract2 {
 
-
-/// @title A system that stores and verifies an a songwriter's work.
-/// @author Tanner Davis 
-/// @notice This is not the most practical way of achieving the goal set in the README.md. 
-/// @notice I plan to implent filecoin or NFTs to make it gas efficient and more straightforward.
-
-contract SongBook {
-    
-    address public owner;
-
-
-    constructor() {
-        owner = msg.sender;
-    }
-
-
-    modifier onlyOwner() {
-      require(msg.sender == owner, "Not owner");
-        
-        _;
-    }
-/// @notice An extra layer of proof that no one can alter the contract.
-   function renounceOwnership() public onlyOwner {
-   }
-    
-     uint public SongID;
-     uint public UserID;
-    
-    
-    event songAdded(uint SongID);
-
-    
-    struct song {
-        string hash;
-        uint SongID;
-    }
-
-
-
-  struct user {
-        string name;
-        uint UserId;
-    }
-    
-    
-  mapping(address => user) public Users;
   
-  mapping(uint => address) public UserIDs;
-  
-  mapping(uint => song) public Songs;
-  
-  
-mapping(address => mapping(string => bool)) public nested;
+mapping(address => mapping(bytes32 => bool)) public nested;
 
-function verifySong1(string memory SongID1) public view returns (bool) {
+function verifySong1(bytes32 SongID1) public view returns (bool) {
 
         address _addr;
 
@@ -69,42 +19,25 @@ function verifySong1(string memory SongID1) public view returns (bool) {
     }
 
     function addSong1(
-        string memory SongID1) public payable {  
+        bytes32 SongID1) public payable { 
+        
         bool _boo;
         _boo = true;
         address _addr;
         _addr = msg.sender;
 
      nested[_addr][SongID1] = _boo;
+     
     }
 
-    function remove(address _addr, string memory SongID1) public {
+    function remove(address _addr, bytes32 SongID1) public {
         delete nested[_addr][SongID1];
     }
 
-/// @notice   Adds a new member to the contract. 
-/// @notice   Referenced in the verifySongfunction.
-  function addUser(string memory _name) public {
-        UserID += 1;
-        Users[msg.sender].name = _name;
-        UserIDs[UserID] = msg.sender;
-    }
     
 
-/// @notice Allows user to log a song to the contract
-/// @notice via a hash of their audio file.
-  function addSong(string memory _hash) public payable returns (uint) {
-        SongID += 1;
-        Songs[SongID].hash = _hash;
-        emit songAdded(SongID);
-        return SongID;
-    }
 
+    
 
-/// @notice Demonstrates that the file hash and user match.
-   function verifySong(string memory _hash, uint _userID) public {
-      Users[msg.sender].UserId = _userID;
-      Songs[SongID].hash = _hash;
-  }
     
 }
--- test/test_SongBook.js
+++ test/test_SongBook.js
@@ -1,76 +1,84 @@
-const SongBook = artifacts.require("SongBook");
+const contract2 = artifacts.require("contract2");
 
 /*
  * uncomment accounts to access the test accounts made available by the
  * Ethereum client
  * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
  */
-contract("SongBook", function ( accounts ) {
-
-  it("allows the owner to renounce ownership", async () => {
-    const ssInstance = await SongBook.deployed();
-
-    await ssInstance.renounceOwnership()
-    
-    return assert.isTrue(true);
-
-  });
+contract("contract2", function ( accounts ) {
 
 
 
-
-
-  it("should assert true", async function () {
-    await SongBook.deployed();
+  it("should deploy", async function () {
+    await contract2.deployed();
     return assert.isTrue(true);
   });
 
 
-  it("should add user", async () => {
-    
-    const ssInstance = await SongBook.deployed();
 
-    await ssInstance.addUser('Tanner');
-
-    return assert.isTrue(true);
-
-
-  
-    
-  });
    
 
   it("should log a song", async () => {
-    const ssInstance = await SongBook.deployed();
+
+    const Instance = await contract2.deployed();
   
 
-    await ssInstance.addSong('ad427c2e80c621ba5b329623d081240ad6405346fe244a50efacf1fb14c7c23e', {from: accounts [1]});
+    await Instance.addSong1('0xad427c2e80c621ba5b329623d081240ad6405346fe244a50efacf1fb14c7c23e', {from: accounts [1]});
 
-    const addedSong = 'ad427c2e80c621ba5b329623d081240ad6405346fe244a50efacf1fb14c7c23e';
+    const addedSong1 = '0xad427c2e80c621ba5b329623d081240ad6405346fe244a50efacf1fb14c7c23e';
     
-    assert.equal(addedSong, 'ad427c2e80c621ba5b329623d081240ad6405346fe244a50efacf1fb14c7c23e', `song not stored`);
+    assert.equal(addedSong1, '0xad427c2e80c621ba5b329623d081240ad6405346fe244a50efacf1fb14c7c23e', `song not stored`);
     
   });
 
 
 
 
-  it("should verify owner", async () => {
+  it("should verify if song is logged to chain", async () => {
 
 
-    const ssInstance = await SongBook.deployed();
+    const Instance = await contract2.deployed();
 
-    await ssInstance.verifySong('ad427c2e80c621ba5b329623d081240ad6405346fe244a50efacf1fb14c7c23e', 5, {from: accounts [1]});
+    await Instance.addSong1('0xad427c2e80c621ba5b329623d081240ad6405346fe244a50efacf1fb14c7c23e', {from: accounts [1]});
 
-    const addedSong = 'ad427c2e80c621ba5b329623d081240ad6405346fe244a50efacf1fb14c7c23e'
-    
-    assert.equal(addedSong, 'ad427c2e80c621ba5b329623d081240ad6405346fe244a50efacf1fb14c7c23e', `song not stored`);
+
+    const verify = await Instance.verifySong1('0xad427c2e80c621ba5b329623d081240ad6405346fe244a50efacf1fb14c7c23e', {from: accounts [1]});
+
+   
+   
+    assert.equal(verify, true);
 
 
 
   });
 
 
+  it("should allow owner to remove their song", async () => {
+    
+    const Instance = await contract2.deployed();
+
+    await Instance.addSong1('0xad427c2e80c621ba5b329623d081240ad6405346fe244a50efacf1fb14c7c23e', {from: accounts [1]});
+
+    const verify1 = await Instance.verifySong1('0xad427c2e80c621ba5b329623d081240ad6405346fe244a50efacf1fb14c7c23e', {from: accounts [1]});
+
+    await Instance.remove(accounts [1], '0xad427c2e80c621ba5b329623d081240ad6405346fe244a50efacf1fb14c7c23e');
+
+    const verify2 = await Instance.verifySong1('0xad427c2e80c621ba5b329623d081240ad6405346fe244a50efacf1fb14c7c23e', {from: accounts [1]});
+
+    assert.equal(false, verify2);
+
+});
+
+  it("should allow user to verify if a song is logged without sending a transaction" , async () => {
+
+    const Instance = await contract2.deployed();
+
+    await Instance.addSong1('0xad427c2e80c621ba5b329623d081240ad6405346fe244a50efacf1fb14c7c23e', {from: accounts [1]});
+
+    const nest = await Instance.nested(accounts [1], '0xad427c2e80c621ba5b329623d081240ad6405346fe244a50efacf1fb14c7c23e')
+
+    assert.equal(nest, true);
+  });
 
 
 });
