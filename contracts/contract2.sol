58df8a11bf0a00ef5b9e10af1e3328c77e47c24e
tannerdavis39 <90158976+tannerdavis39@users.noreply.github.com>
2022-01-02 19:18:23 -0500

    commented smart contract

--- contracts/contract2.sol
+++ contracts/contract2.sol
@@ -4,11 +4,19 @@ pragma solidity 0.8.10;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+/// @title A Songbook for copyright
+/// @author Tanner Davis
+/// @notice You can use this contract to log and verify the presence of your song.
     contract contract2 {
 
-  
+    /// @notice gets the status of song via .call
+    /// @return boolean that represtents "song added" or "not yet added"
 mapping(address => mapping(bytes32 => bool)) public nested;
 
+
+    /// @notice shows if a song has been added or not
+    /// @param SongID : the SHA-256 hash of the song file.
+    /// @return boolean that represtents "song added" or "not yet added"
 function verifySong1(bytes32 SongID1) public view returns (bool) {
 
         address _addr;
@@ -17,7 +25,8 @@ function verifySong1(bytes32 SongID1) public view returns (bool) {
 
         return nested[_addr][SongID1];
     }
-
+    /// @notice Add a song that's made up of the hash and sender address.
+    /// @param SongID : the SHA-256 hash of the song file.
     function addSong1(
         bytes32 SongID1) public payable { 
         
